b84d1b62a3ceea3e8150aa528fbac3ac0a94c823
unknown <hary.teguh@ui.ac.id>
2021-04-06 02:01:30 +0700

    first commit

---
+++ belajar-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// fmt.Println("Hello", "World", "Test") // printing in Go
+	/* var text string = "Hello" // store var in declared data type
+	secondVar := 3 */ // decalre variable without define it's data type
+	/* var firstVar, secondVar = "Hello", 4 // multiple var declared datatypes
+	thirdVar, forthVar := "Hello", "World" // multiple var undeclared datatypes
+	_ = 5 // uderscore as null is used to store unused variable
+	fmt.Println(firstVar, secondVar) // combine printing var
+	fmt.Println(thirdVar, forthVar) */
+
+}
